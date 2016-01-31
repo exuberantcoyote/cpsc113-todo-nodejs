5f683801021bdb1ec66c89589aa7d0a7894c229e
exuberantcoyote <trenttolley@gmail.com>
2016-01-31 03:02:32 +0000

    added session persistance and password hashing without kyles video

--- server.js
+++ server.js
@@ -4,18 +4,25 @@ var exphbs  = require('express-handlebars');
 var app = express();
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var MongoDBStore = require('connect-mongodb-session')(session);
 var Users = require('./models/users.js');
 
-
 // Configuration
+var store = new MongoDBStore({ 
+  uri: process.env.MONGO_URL,
+  collection: 'sessions'
+});
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+// Middleware
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: 'auto' }
+  cookie: { secure: 'auto' },
+  store: store
 }))
 
 app.use(function(req, res, next){
@@ -41,6 +48,7 @@ app.use(function(req, res, next){
   }
 })
 
+
 // Controllers
 app.get('/', function (req, res) {
 //  res.render('index');
@@ -60,24 +68,50 @@ app.post('/user/register', function (req, res) {
   if(req.body.password != req.body.password_confirmation){
     return res.render('index', {errors: 'Password does not match'});
   }
-  
   var newUser = new Users();
   newUser.hashed_pwrd = req.body.password;
   newUser.email = req.body.email;
   newUser.name = req.body.fl_name;
   newUser.save(function(err, user){
-    req.session.userId = user._id;
+    //req.session.userId = user._id;
     //console.log('added new user ' + user)
     if(err){
+      err = 'Error registering user.';
       res.render('index', {errors: err});
       //res.send('there was an error saving this user');
     }else{
+      req.session.userId = user._id;
       res.redirect('/');
     }
   })
-  
   //res.send(req.body);
 });
+
+app.post('/user/login', function (req, res) {
+  var user = Users.findOne({email: req.body.email}, function(err, user){
+    if(err || !user){
+      res.send('bad login, no such user');
+      return;
+    }
+    console.log('user =', user);
+    console.log('actual password =', user.hashed_pwrd);
+    console.log('provided password =', req.body.password);
+    
+    user.comparePassword(req.body.password, function(err, isMatch){
+      if(err || !isMatch){
+        res.send('incorrect password');
+      }else{
+        req.session.userId = user._id;
+        res.redirect('/')
+      }
+    });
+  })
+});
+
+app.get('/user/logout', function(req, res){
+  req.session.destroy();
+  res.redirect('/');
+})
 
 app.listen(process.env.PORT, function () {
   console.log('Example app listening on port ' + process.env.PORT);
